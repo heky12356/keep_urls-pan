21b5a2f635582167af0fb50361e90078acc4feaa
heky <heky@example.com>
2024-02-10 23:07:06 +0800

    1

---
+++ README.md
@@ -0,0 +1,2 @@
+keep_urls'pan
+一个用来保存网址的web应用
---
+++ requireurl.php
@@ -0,0 +1,17 @@
+<?php
+// 连接 SQLite 数据库
+$db = new SQLite3('urldb.db');
+
+// 执行查询
+$queryResult = $db->query('SELECT * FROM urls');
+
+// 将结果转换为数组
+$resultsArray = array();
+while ($row = $queryResult->fetchArray(SQLITE3_ASSOC)) {
+    $resultsArray[] = $row;
+}
+
+// 返回 JSON 格式的结果
+header('Content-Type: application/json');
+echo json_encode($resultsArray);
+?>
